efdd7fef5b37eacc273f16782defdc53dbbb4683
ql0320 <qluo0320@gmail.com>
2023-11-08 15:26:59 -0500

    Removed unused capacity factor setting
    1. remove the setting on capacity factor from settings file;
    2. enable writing capacity retrofit only when there is retrofit resources;
    3. update README.md for the new example case

--- src/write_outputs/write_capacity_retrofit.jl
+++ src/write_outputs/write_capacity_retrofit.jl
@@ -9,55 +9,7 @@ function write_capacity_retrofit(path::AbstractString, inputs::Dict, setup::Dict
 	RETRO_SOURCE_IDS = inputs["RETROFIT_SOURCE_IDS"] # Source technologies by ID for each retrofit [1:G]
 	RETRO_EFFICIENCY = inputs["RETROFIT_EFFICIENCIES"]
 	NUM_RETRO_SOURCES = inputs["NUM_RETROFIT_SOURCES"]
-
-	# MultiStage = setup["MultiStage"]
 	
-	# capdischarge = zeros(size(inputs["RESOURCES"]))
-	# for i in inputs["NEW_CAP"]
-	# 	if i in inputs["COMMIT"]
-	# 		capdischarge[i] = value(EP[:vCAP][i])*dfGen[!,:Cap_Size][i]
-	# 	else
-	# 		capdischarge[i] = value(EP[:vCAP][i])
-	# 	end
-	# end
-
-	# retcapdischarge = zeros(size(inputs["RESOURCES"]))
-	# for i in inputs["RET_CAP"]
-	# 	if i in inputs["COMMIT"]
-	# 		retcapdischarge[i] = first(value.(EP[:vRETCAP][i]))*dfGen[!,:Cap_Size][i]
-	# 	else
-	# 		retcapdischarge[i] = first(value.(EP[:vRETCAP][i]))
-	# 	end
-	# end
-
-	# capcharge = zeros(size(inputs["RESOURCES"]))
-	# retcapcharge = zeros(size(inputs["RESOURCES"]))
-	# existingcapcharge = zeros(size(inputs["RESOURCES"]))
-	# for i in inputs["STOR_ASYMMETRIC"]
-	# 	if i in inputs["NEW_CAP_CHARGE"]
-	# 		capcharge[i] = value(EP[:vCAPCHARGE][i])
-	# 	end
-	# 	if i in inputs["RET_CAP_CHARGE"]
-	# 		retcapcharge[i] = value(EP[:vRETCAPCHARGE][i])
-	# 	end
-	# 	existingcapcharge[i] = MultiStage == 1 ? value(EP[:vEXISTINGCAPCHARGE][i]) : dfGen[!,:Existing_Charge_Cap_MW][i]
-	# end
-
-	# capenergy = zeros(size(inputs["RESOURCES"]))
-	# retcapenergy = zeros(size(inputs["RESOURCES"]))
-	# existingcapenergy = zeros(size(inputs["RESOURCES"]))
-	# for i in inputs["STOR_ALL"]
-	# 	if i in inputs["NEW_CAP_ENERGY"]
-	# 		capenergy[i] = value(EP[:vCAPENERGY][i])
-	# 	end
-	# 	if i in inputs["RET_CAP_ENERGY"]
-	# 		retcapenergy[i] = value(EP[:vRETCAPENERGY][i])
-	# 	end
-	# 	existingcapenergy[i] = MultiStage == 1 ? value(EP[:vEXISTINGCAPENERGY][i]) :  dfGen[!,:Existing_Cap_MWh][i]
-	# end
-
-	# RETRO_SOURCE_IDS[r][i] in RET_CAP ? RETRO_EFFICIENCY[r][i] : 0 for i in 1:NUM_RETRO_SOURCES[r]; init=0 
-
 	RETROFIT_SOURCE = [];
 	RETROFIT_DEST = [];
 	RETROFIT_CAP = [];
