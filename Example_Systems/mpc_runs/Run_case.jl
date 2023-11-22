cad665e8bac1085b0fdc51d8836683eed244015f
Les A <lesgarm@mit.edu>
2023-11-21 23:14:54 -0500

    Updated initial state of charge to be matched across runs

--- Example_Systems/mpc_runs/Run_case.jl
+++ Example_Systems/mpc_runs/Run_case.jl
@@ -24,8 +24,10 @@ keep_hrs = 24 * 7
 ## This gives you the number of runs
 run_number = max_hrs / keep_hrs
 
+iSoC = Dict()
 ####### This is setup for Supercloud so make sure to change your paths accordingly ########
 for i in 1:run_number
+
     case = run_path
 
     function get_settings_path(case::AbstractString)
@@ -84,11 +86,11 @@ for i in 1:run_number
     #println(fuels_window)
     fuels_time = collect(start_index:end_index)
     #insert!(fuels_time, 1, 0)
-    println(fuels_time)
+    #println(fuels_time)
 
     fuels_window[!, "Time_Index"] = fuels_time
     insert!(fuels_window, 1, fuels_one)
-    println(fuels_window)
+    #println(fuels_window)
 
     CSV.write(joinpath(rolling_horizon_data_path, "Fuels_data.csv"), fuels_window)
 
@@ -99,7 +101,7 @@ for i in 1:run_number
     genvar_filter = genvar_data[start_index:end_index, :]
     genvar_time = collect(start_index:end_index)
     genvar_filter[!, "Time_Index"] = genvar_time
-    println(genvar_filter)
+    #println(genvar_filter)
 
 
     CSV.write(joinpath(rolling_horizon_data_path, "Generators_variability.csv"), genvar_filter)
@@ -118,14 +120,58 @@ for i in 1:run_number
     println("Generating the Optimization Model")
     EP = generate_model(mysetup, myinputs, OPTIMIZER)
 
+    ## Constraint initial state of charge variable setup
+    if i != 1
+        dfGen = myinputs["dfGen"]
+        Reserves = mysetup["Reserves"]
+
+        G = myinputs["G"]     # Number of resources (generators, storage, DR, and DERs)
+        T = myinputs["T"]     # Number of time steps (hours)
+        Z = myinputs["Z"]     # Number of zones
+
+        STOR_ALL = myinputs["STOR_ALL"]
+        STOR_SHORT_DURATION = myinputs["STOR_SHORT_DURATION"]
+        representative_periods = myinputs["REP_PERIOD"]
+
+        START_SUBPERIODS = myinputs["START_SUBPERIODS"]
+        INTERIOR_SUBPERIODS = myinputs["INTERIOR_SUBPERIODS"]
+
+        hours_per_subperiod = myinputs["hours_per_subperiod"] #total number of hours per subperiod
+
+        ## Constraint initial state of charge
+        @constraints(EP, begin
+        cSoCInitial[y in STOR_ALL], EP[:vS][y, 1] == iSoC[y]
+        end)
+    end
+
     println("Solving Model")
     EP, solve_time = solve_model(EP, mysetup)
     myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
 
-    # Run MGA if the MGA flag is set to 1 else only save the least cost solution
-    println("Writing Output")
+    # Run MGA if the MGA flag is set to 1 else only save the least cost solutionsho    println("Writing Output")
     outputs_path = joinpath(runs_results_path, "Run_$i")
     elapsed_time = @elapsed write_outputs(EP, outputs_path, mysetup, myinputs)
     println("Time elapsed for writing Run_$i is")
     println(elapsed_time)
+
+    STOR_ALL = myinputs["STOR_ALL"]
+    dfGen = myinputs["dfGen"]
+
+    
+    for y in STOR_ALL
+        iSoC[y] = value(EP[:vS][y,window]) - (dfGen[y,:Self_Disch]*value(EP[:vS][y,window]))
+    end
+    println(iSoC)
+
 end
+
+# Setting initial State of Charge (iSoC). If first run set to zero. Otherwise set to the previous model window results
+# if i == 1
+#     # Pass
+#     println(0)
+# else
+#     for y in STOR_ALL
+#         iSoC[y] = EP[:vS][y,window]-(dfGen[y,:Self_Disch]*EP[:vS][y,window])
+#     end
+#     println(iSoC)
+# end
